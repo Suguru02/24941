cf63db4b8143b66629f2598b05151ad10733d039
Anton Bykov <a.bykov4@g.nsu.ru>
2025-10-27 23:57:38 +0700

    added 9 and 10

---
+++ a.bykov4/10task/10_task.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char* argv[]){
+
+    if (argc < 2){
+        perror("argc");
+        return 1;
+    }
+
+    pid_t descendant_PID;
+
+    switch(descendant_PID=fork()) {
+    case -1:
+        perror("fork"); /* произошла ошибка */
+        exit(1); /*выход из родительского процесса*/
+    case 0:
+        execvp(argv[1], &argv[1]);
+        perror("execvp");
+        return 1;
+    default:
+        int status;
+        wait(&status);
+        printf("\nstatus = %d\n", status);
+    }
+
+    return 0;
+}
